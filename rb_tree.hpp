611c3c18af3b0f0e020f659233cfbe7656afc049
William Naciri <dontbl1nk@users.noreply.github.com>
2018-09-03 20:55:03 +0000

    finished insert

--- rb_tree.hpp
+++ rb_tree.hpp
@@ -1,5 +1,6 @@
 #ifndef RB_TREE_HPP
 #define RB_TREE_HPP
+#include <iostream>
 #include <stdexcept>
 
 template <class T>
@@ -11,37 +12,115 @@ private:
     public:
         Node *left, *right, *parent;
         Color color;
-        Node(Node *left, Node *right, Color color): left(left), right(right), parent(nullptr), color(color){};
-        static Node* sentinel(){
+        Node(Node* left, Node* right, Color color) : left(left), right(right), parent(nullptr), color(color){};
+        static Node* sentinel() {
             static SentinelNode sentinel;
             static Node* sentinel_ptr(static_cast<Node*>(static_cast<void*>(&sentinel)));
             return sentinel_ptr;
         }
         // TODO: Remove get_val when debugging is complete
-        virtual T get_val(){
-            throw std::runtime_error("Not an implmentation of Node");
-        }
+        virtual T get_val() { throw std::runtime_error("Not an implmentation of Node"); }
     };
-    class SentinelNode: public Node {
+    class SentinelNode : public Node {
     public:
-        SentinelNode(): Node(nullptr, nullptr, Color::black) {}
-        T get_val() {
-            throw std::runtime_error("Accessing sentinel value");
-        }
+        SentinelNode() : Node(nullptr, nullptr, Color::black) {}
+        T get_val() { throw std::runtime_error("Accessing sentinel value"); }
     };
-    class ValueNode: public Node {
+    class ValueNode : public Node {
     private:
         T val;
+
     public:
-        ValueNode(T val): Node(Node::sentinel(), Node::sentinel(), Color::red), val(val){}
-        T get_val() {
-            return val;
-        }
+        ValueNode(T val) : Node(Node::sentinel(), Node::sentinel(), Color::red), val(val) {}
+        T get_val() { return val; }
     };
     /* ===== RBT ===== */
     Node* root;
 
-    static Node* rb_insert_fixup(Node* z) { return z; }
+    static Node* rb_insert_fixup_case1(Node* grandparent, Node* parent, Node* child) {
+        grandparent->left = parent->right;
+        grandparent->left->parent = grandparent;
+        parent->right = grandparent;
+        parent->right->parent = parent;
+        grandparent->color = Color::red;
+        parent->color = Color::black;
+        return parent;
+    }
+    static Node* rb_insert_fixup_case2(Node* grandparent, Node* parent, Node* child) {
+        grandparent->left = Node::sentinel();
+        parent->right = Node::sentinel();
+        child->left = parent;
+        child->left->parent = child;
+        child->right = grandparent;
+        child->right->parent = child;
+        grandparent->color = Color::red;
+        child->color = Color::black;
+        return child;
+    }
+    static Node* rb_insert_fixup_case3(Node* grandparent, Node* parent, Node* child) {
+        grandparent->right = parent->left;
+        grandparent->right->parent = grandparent;
+        parent->left = grandparent;
+        parent->left->parent = parent;
+        grandparent->color = Color::red;
+        parent->color = Color::black;
+        return parent;
+    }
+    static Node* rb_insert_fixup_case4(Node* grandparent, Node* parent, Node* child) {
+        grandparent->right = Node::sentinel();
+        parent->left = Node::sentinel();
+        child->right = parent;
+        child->right->parent = child;
+        child->left = grandparent;
+        child->left->parent = child;
+        grandparent->color = Color::red;
+        parent->color = Color::black;
+        return child;
+    }
+    static Node* rb_insert_fixup_case5(Node* grandparent, Node* parent, Node* uncle) {
+        grandparent->color = Color::red;
+        parent->color = Color::black;
+        uncle->color = Color::black;
+        return grandparent;
+    }
+    static Node* rb_insert_fixup_left(Node* grandparent) {
+        Node* parent(grandparent->left);
+        Node* uncle(grandparent->right);
+        if (parent->color == Color::black) {
+            return grandparent;
+        } else if (parent->left->color == Color::red) {
+            if (uncle->color == Color::red) {
+                return rb_insert_fixup_case5(grandparent, parent, uncle);
+            }
+            return rb_insert_fixup_case1(grandparent, parent, parent->left);
+        } else if (parent->right->color == Color::red) {
+            if (uncle->color == Color::red) {
+                return rb_insert_fixup_case5(grandparent, parent, uncle);
+            }
+            return rb_insert_fixup_case2(grandparent, parent, parent->right);
+        } else {
+            return grandparent;
+        }
+    }
+    static Node* rb_insert_fixup_right(Node* grandparent) {
+        Node* parent(grandparent->right);
+        Node* uncle(grandparent->left);
+        if (parent->color == Color::black) {
+            return grandparent;
+        } else if (parent->left->color == Color::red) {
+            if (uncle->color == Color::red) {
+                return rb_insert_fixup_case5(grandparent, parent, uncle);
+            }
+            return rb_insert_fixup_case4(grandparent, parent, parent->left);
+        } else if (parent->right->color == Color::red) {
+            if (uncle->color == Color::red) {
+                return rb_insert_fixup_case5(grandparent, parent, uncle);
+            }
+            return rb_insert_fixup_case3(grandparent, parent, parent->right);
+        } else {
+            return grandparent;
+        }
+    }
 
     static Node* rb_insert(Node* root, Node* z) {
         if (!root->left) {
@@ -49,11 +128,13 @@ private:
         } else if (z->get_val() < root->get_val()) {
             root->left = rb_insert(root->left, z);
             root->left->parent = root;
+            root = rb_insert_fixup_left(root);
         } else {
             root->right = rb_insert(root->right, z);
             root->right->parent = root;
+            root = rb_insert_fixup_right(root);
         }
-        return rb_insert_fixup(root);
+        return root;
     }
     static bool rb_find(Node* root, const T& val) {
         if (!root->left) {
@@ -72,6 +153,7 @@ public:
     void insert(const T& val) {
         Node* z = new ValueNode(val);
         root = rb_insert(root, z);
+
         root->color = Color::black;
     }
     bool find(const T& val) { return rb_find(root, val); }
--- test.cpp
+++ test.cpp
@@ -11,10 +11,16 @@ BOOST_AUTO_TEST_CASE(test1) {
     RBTree<int> rbt;
     rbt.insert(1);
     rbt.insert(2);
-    rbt.insert(5);
     rbt.insert(3);
     rbt.insert(4);
-    BOOST_TEST(rbt.find(6) == false);
+    rbt.insert(5);
+    rbt.insert(6);
+    rbt.insert(7);
+    rbt.insert(8);
+    rbt.insert(9);
+    rbt.insert(10);
+    BOOST_TEST(rbt.find(0) == false);
+    BOOST_TEST(rbt.find(10) == true);
 }
 
 /* BOOST_AUTO_TEST_CASE(test1) {
